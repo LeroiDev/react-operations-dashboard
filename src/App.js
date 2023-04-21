e4ed95969b7a6e368e8348935d874f0a20e9704f
Leroi <67743669+LeroiDev@users.noreply.github.com>
2023-04-21 16:54:26 +0200

    active-menu-sidebar-next

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { BrowserRouter,Routes,Route} from 'react-router-dom'
+import { FiSettings } from 'react-icons/fi'
+import { Tooltip, TooltipComponent } from '@syncfusion/ej2-react-popups'
 
 const App = () => {
+  const activeMenu = true;
   return (
-    <h1 className='underline text-3xl'>WHAT IS UP REACT</h1>
+    <BrowserRouter>
+      <div className='flex relative dark:bg-main-dark-bg'>
+        <div className='fixed right-4 bottom-4' style={{zIndex:1000}}>
+          <TooltipComponent content="Settings" position="Top">
+            <button type='button' className='text-3xl p-3 hover:drop-shadow-x1 hover:bg-light-gray text-white' 
+            style={{background: 'blue', borderRadius:'50%'}}>
+              <FiSettings></FiSettings>
+            </button>
+          </TooltipComponent>
+        </div>
+        {activeMenu ? (
+          <div>
+              sidebar
+          </div>
+        ):
+          (
+            <div>
+              sidebar
+            </div>
+          )
+        }
+      </div>
+    </BrowserRouter>
   )
 }
 export default App
